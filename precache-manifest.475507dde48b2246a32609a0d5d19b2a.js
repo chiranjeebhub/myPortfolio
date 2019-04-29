31a210e6fec539b33342b4a3b9d492284f33b787
Chiranjeeb Jena <chiranjib.jena@gmail.com>
2019-04-29 09:04:38 +0530

    Updates

---
+++ precache-manifest.475507dde48b2246a32609a0d5d19b2a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8fd36a7383e94b543638810b28a321",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "aa1df98b2b6de07ef3b1",
+    "url": "/myPortfolio/static/css/2.454e6180.chunk.css"
+  },
+  {
+    "revision": "ea27c77031e6fb69f672",
+    "url": "/myPortfolio/static/css/main.06982276.chunk.css"
+  },
+  {
+    "revision": "aa1df98b2b6de07ef3b1",
+    "url": "/myPortfolio/static/js/2.9ad8e065.chunk.js"
+  },
+  {
+    "revision": "ea27c77031e6fb69f672",
+    "url": "/myPortfolio/static/js/main.76b90eec.chunk.js"
+  },
+  {
+    "revision": "2261391d3370eb3cf5eb",
+    "url": "/myPortfolio/static/js/runtime~main.918aa22c.js"
+  },
+  {
+    "revision": "53206b6e4670c05c993016f4fbcbd690",
+    "url": "/myPortfolio/static/media/logo.53206b6e.jpg"
+  },
+  {
+    "revision": "90b51781d291c272ea706637d577dd4a",
+    "url": "/myPortfolio/static/media/me.90b51781.jpg"
+  }
+]);
